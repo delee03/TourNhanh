d3bce79f41348a3f4bd3def434b0e3b179a7b2fc
Pham Phat <phamphat2003vn@gmail.com>
2024-04-22 21:52:15 +0700

    first commit

---
+++ TourNhanh/Controllers/InternationalTourController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourNhanh.Controllers
+{
+    public class InternationalTourController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
