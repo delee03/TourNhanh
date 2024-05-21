c4a20377107de88b3b366cfe33d170f4b7eaeefc
NLSon03 <nlson.2003.nguyen@gmail.com>
2024-05-21 11:50:27 +0700

    ..

--- TourNhanh/obj/Debug/net8.0/TourNhanh.AssemblyInfo.cs
+++ TourNhanh/obj/Debug/net8.0/TourNhanh.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourNhanh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc4aedaef560b3d6f94e2f72cdbf8eecb30ceb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812f2bc31d238fefe06671721e6f70f765a1485b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourNhanh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourNhanh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
