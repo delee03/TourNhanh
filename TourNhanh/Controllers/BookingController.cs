4d08abfff6da19601ec51599036d27d058e12153
NLSon03 <nlson.2003.nguyen@gmail.com>
2024-05-23 13:04:38 +0700

    ..

---
+++ TourNhanh/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourNhanh.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
