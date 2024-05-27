f1699a7c5c4221b2cf029c813f8e9dc5240d9ac4
Pham Phat <phamphat2003vn@gmail.com>
2024-05-27 13:08:07 +0700

    add button Duyệt Bài

---
+++ TourNhanh/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourNhanh.Areas.Admin.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
